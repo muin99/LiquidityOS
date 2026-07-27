f3324083d32490531e580d5de9d0d87b6e5223f7
MD. Mohaiminul Haque <tashinggara129@gmail.com>
2026-07-28 00:11:07 +0600

    Added Users module, authentication, RABC authorization, login, register JWT authentication

--- liquidityos/src/common/decorators/roles.decorator.ts
+++ liquidityos/src/common/decorators/roles.decorator.ts
@@ -1,5 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../enums/domain.enums';
+import { UserRole } from '../../users/enums/user-role.enum';
 
 export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
+
+export const Roles = (...roles: UserRole[]) =>
+  SetMetadata(ROLES_KEY, roles);
---
+++ liquidityos/src/users/enums/user-role.enum.ts
@@ -0,0 +1,5 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  PROVIDER = 'provider',
+  AGENT = 'agent',
+}
