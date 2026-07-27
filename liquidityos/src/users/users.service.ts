dc1e76f807045a3daea1ed06b883a0db75009c5f
MD. Mohaiminul Haque <tashinggara129@gmail.com>
2026-07-28 02:57:47 +0600

    Added Phone number as alternative identifier

--- liquidityos/src/users/users.service.ts
+++ liquidityos/src/users/users.service.ts
@@ -31,6 +31,10 @@ export class UsersService {
     });
   }
 
+  async findByPhone(phone: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { phone } });
+  }
+
   async update(id: string, data: Partial<User>): Promise<User | null> {
     await this.usersRepository.update(id, data);
 
